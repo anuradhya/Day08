8092d757f8dadc69bb1ade535dff110318046a1f
W.L.A.D.D.A. Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2024-12-01 14:48:31 +0000

    Ceated the needed components

---
+++ myapp/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink
+}from "react-router-dom";
+
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/aboutus" element ={<AboutUs/>}/>
+        <Route path="contsctus" element={<ContactUs/>}/>
+        <Route path="dashboard" element={<Dashboard/>}/>
+        <Route path="home" element={<Home/>}/>
+        <Route path="login" element={<Login/>}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/ContactUs.js
@@ -0,0 +1,7 @@
+export default function ContactUs(){
+    return(
+        <>
+        <h1>Welcome to Contact us Page</h1>
+        </>
+    )
+}
