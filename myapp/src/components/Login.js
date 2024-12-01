370b5fe71bd2a0283edec90ec388fb84469a9a1e
W.L.A.D.D.A. Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2024-12-01 14:59:13 +0000

    Update handlechange in Login.js

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,4 +1,26 @@
+import {Studentdata} from '../data/data';
+import { useState } from "react";
+import {Link,useNavigate} from "react-router-dom";
+
 export default function Login(){
+    const navigate=useNavigate();
+
+    const [Credential,setCredentials]=useState({
+        username: "",
+        password: "",
+        error: "",
+    });
+
+    const handlechange=(e)=>{
+        e.preventDefault();
+        const {name, value}=e.target;
+        setCredentials((prev)=>({
+            ...prev,
+            [name]:value,
+        }))
+    }
+
+
     return(
         <>
         </>
